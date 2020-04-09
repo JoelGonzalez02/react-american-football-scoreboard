9b3b762f177bf9366e97732e24cfbd6069243ace
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-04-08 19:02:12 -0700

    Made Changes to Stretch goals

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+
 const BottomRow = () => {
+
+  
+const [quarter, setQuarter] = useState(1);
+
+const changeQuarter = (e) => {
+  setQuarter(quarter + 1);
+};
+
+
   return (
     <div className="bottomRow">
       <div className="down">
-        <h3 className="down__title">Down</h3>
+        <h3 className="down__title">Home Timeouts</h3>
         <div className="down__value">3</div>
       </div>
       <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <h3 className="toGo__title">Away Timeouts</h3>
+        <div className="toGo__value">3</div>
       </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <h3 className="ballOn__title">Shot Clock</h3>
+        <div className="ballOn__value">24</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+  <div className="quarter__value">{quarter}</div>
+  <button className ="newQuarter" onClick = {changeQuarter}>Change Quarter</button>
       </div>
     </div>
   );
